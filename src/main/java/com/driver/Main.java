6f84e816eb8dcee1d5f729b558288cc328eba2e8
Magan Sharma <magansharma8122@gmail.com>
2023-01-23 00:44:29 +0530

    overriding1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,29 +1,11 @@
 package com.driver;
-class A{
-    public A() {
-    }
-
-    String meth(){
-        return "Invoking method from class A";
-    }
-}
-class B extends A{
-    public B() {
-    }
-
-    @Override
-    String meth() {
-        return "Method is overridden in Extendend class B";
-    }
-}
 
 
 public class Main {
 
     public static void main(String[] args) {
-        B b= new B();
-        b.meth();
-       // c.meth();
-        // 2nd sol
+      B b= new B();
+      b.meth();
+
     }
 }
